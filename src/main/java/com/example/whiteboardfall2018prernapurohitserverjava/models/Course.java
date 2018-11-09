6acbe013a657a010f73ae817d9a5c4a5639bdeaa
prerna-p <prernasocialid@gmail.com>
2018-11-08 22:26:59 -0500

    completed course model

--- src/main/java/com/example/whiteboardfall2018prernapurohitserverjava/models/Course.java
+++ src/main/java/com/example/whiteboardfall2018prernapurohitserverjava/models/Course.java
@@ -1,12 +1,16 @@
 package com.example.whiteboardfall2018prernapurohitserverjava.models;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Course {
@@ -14,7 +18,13 @@ public class Course {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String title;
-	private List<Module> modules = new ArrayList<Module>();
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date modified;
+	@OneToMany(mappedBy="course")
+	private List<Module> modules;
+	
 	public Course(String title) {
 		this.title = title;
 	 }
@@ -40,5 +50,16 @@ public class Course {
 	public void setModules(List<Module> modules) {
 		this.modules = modules;
 	}
-	  
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getModified() {
+		return modified;
+	}
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
 }
